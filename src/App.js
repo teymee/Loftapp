4911ad7af15f44a7fda0b1c761af9743fd982b32
Okorie Emmanuel <teymee17@gmail.com>
2022-03-27 15:20:34 +0100

    Logo change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Soon from './Pages/Coming-soon/Coming-soon';
 
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.svg";
 
 import './nav.css'
 
 function Nav() {
 	return (
 		<div className="nav">
-			<div></div>
 			<img src={Logo} alt="Logo" />
-                  <span>LOFT</span>
 		</div>
 	);
 }
